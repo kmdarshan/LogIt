c2d616e85eb4ca60042f782ea0d6a57af54436f5
darshan katrumane <kmdarshan@gmail.com>
2015-02-14 13:00:49 -0800

    Initial Commit

---
+++ LogIt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LogIt
+//
+//  Created by kmd on 2/14/15.
+//  Copyright (c) 2015 Lyft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
